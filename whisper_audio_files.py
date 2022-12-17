f3d8d31ef6909ae229d8559f43af096e955fb090
Onur Sarikaya <mail@onursarikaya.com>
2022-12-17 21:04:28 +0100

    added check so that already transcribed files won't get transcribed again

--- whisper_audio_files.py
+++ whisper_audio_files.py
@@ -11,12 +11,14 @@ if not os.path.exists(txt_dir):
 
 for mp3 in os.listdir(mp3_dir):
     mp3_filename = os.fsdecode(mp3)
-    if mp3_filename.endswith(".mp3"):
+    txt_file_name = mp3_filename.replace('mp3', 'txt')
+    txt_file_path = os.path.join(txt_dir, txt_file_name)
+
+    if mp3_filename.endswith(".mp3") and os.path.isfile(txt_file_path) == False:
         result = model.transcribe(os.path.join(
             mp3_dir, mp3_filename), language='de', fp16=False)
-        txt_file_name = mp3_filename.replace('mp3', 'txt')
 
-        text_file = open(os.path.join(txt_dir, txt_file_name), "w")
+        text_file = open(txt_file_path, "w")
         text_file.write(result['text'])
         text_file.close()
         continue
